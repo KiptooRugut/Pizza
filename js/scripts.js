d9cc7037bf28e82f28bece8510b7c34baa0c21a2
Kiptoo Rugut <mccrug97@gmail.com>
2022-01-16 20:52:27 +0300

    it will add a function that executes when customers selects pick option

--- js/scripts.js
+++ js/scripts.js
@@ -4,3 +4,12 @@ function Total(size, crust, topping, delivery) {
     this.topping = topping;
     this.delivery = delivery;
 }
+
+function validatePick() {
+    let selectedCrust = parseFloat(document.getElementById("pizza-crust").value);
+    let selectedToppings = parseFloat(document.getElementById("pizza-toppings").value);
+    let selectedSize = parseFloat(document.getElementById("pizza-size").value);
+    let quantity = document.getElementById("pizzaNumber").value;
+    let selectedDelivery = parseFloat(document.getElementById("pick").value);
+
+    const pickDelivery = new Total(selectedSize, selectedCrust, selectedToppings, selectedDelivery);
