fa2f01cc585f6cd742d2a7e6894fedec69e93254
Kiptoo Rugut <mccrug97@gmail.com>
2022-01-16 22:54:58 +0300

    it will add a function that executes when customers selects delivery option

--- js/scripts.js
+++ js/scripts.js
@@ -31,3 +31,35 @@ function validatePick() {
     }
 
 }
+
+function validateDeliver() {
+    let selectedCrust = parseFloat(document.getElementById("pizza-crust").value);
+    let selectedToppings = parseFloat(document.getElementById("pizza-toppings").value);
+    let selectedSize = parseFloat(document.getElementById("pizza-size").value);
+    let selectedDelivery = parseFloat(document.getElementById("deliver").value);
+    let quantity = document.getElementById("pizzaNumber").value;
+
+    const deliver = new Total(selectedSize, selectedCrust, selectedToppings, selectedDelivery);
+
+    var cost = deliver.size + deliver.crust + deliver.topping + deliver.delivery
+    let totalCost = cost * quantity;
+    if (selectedCrust == "" || selectedToppings == "" || selectedSize == "" || quantity == "") {
+        $(".alert").show();
+    } else {
+        $(".alertDeliver").show();
+        if (charge) {
+            $(".order-form").hide();
+            $(".summary").show();
+        } else {
+            return false;
+        }
+
+    }
+    if (charge) {
+        $("#itemQuantity").text(quantity);
+        $("#itemSize").text(selectedSize);
+        $("#itemCrust").text(selectedCrust);
+        $("#itemTopping").text(selectedToppings);
+        $("#totalValue").text(totalCost);
+    }
+}
